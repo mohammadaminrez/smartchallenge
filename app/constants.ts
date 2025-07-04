fa0e8e44dbe59bcebca5921ec174911a95b62508
Amin <mohammadaminrez@gmail.com>
2025-07-04 14:40:25 +0200

    Update constants.ts

--- app/constants.ts
+++ app/constants.ts
@@ -3,4 +3,4 @@
 //export const CONTRACT_ADDRESS = "0x82C999FaD4c93711a0ce86aDC0487e1Dc8Cc62EF";
 //export const CONTRACT_ADDRESS = "0xd5C650F0aa5Acc615dcE94e159fC6554b2de4eC3"; // owner: Ali
 // Reads the contract address from NEXT_PUBLIC_CONTRACT_ADDRESS in .env.local (frontend root). This variable must be set.
-export const CONTRACT_ADDRESS = "0x5475bb7D41924ba084899468447FCf7D7a998677";
+export const CONTRACT_ADDRESS = "0x25464Ce44Ab67EB7f6954e362eF8271E4a6F5c55";
